def0433382ee14477b0ec6f7a994bd2469046092
scooterzman <35939890+scooterzman@users.noreply.github.com>
2019-08-11 18:01:30 -0700

    first commit

---
+++ Script.js
@@ -0,0 +1,17 @@
+document.getElementById("foot01").innerHTML =
+"<p>&copy;  " + new Date().getFullYear() + " Szeman Analytics and Consulting. All rights reserved.</p>";
+
+document.getElementById("nav01").innerHTML =
+ "<ul id='menu'>" +
+	"<li><a href='Index.html'>Home</a></li>" +
+	"<li><a href='Getting Started.html'>Getting Started</a></li>" +
+ 	"<li><a href='Wealth Simulation.html'>Goal Simulation</a></li>" +
+	"<li><a href='Real Estate Modelling.html'>Real Estate</a></li>" +
+	"<li><a href='Personal Budet.html'>Budgeting</a></li>" +
+	"<li><a href='Asset Allocation.html'>Asset Allocation</a></li>" +
+ 	"<li><a href='About.html'>About</a></li>" +
+	"<li><a href='phpPractice.html'>php Practice</a></li>" +
+ "</ul>";
+
+
+
